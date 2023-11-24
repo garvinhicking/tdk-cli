9e45c0bf5adacf390867c13a4603853817c7961a
Garvin Hicking <gh@faktor-e.de>
2023-11-24 15:50:59 +0100

    [BUGFIX] Try to fix autoloading with missing class

--- src/Cli.php
+++ src/Cli.php
@@ -8,6 +8,7 @@ use Composer\Script\Event;
 use Composer\Util\ProcessExecutor;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
 
 class Cli
 {
@@ -24,21 +25,30 @@ class Cli
 
     public static function cloneRepository(Event|OutputInterface $event): bool
     {
-        $filesystem = new Filesystem();
-        if (!$filesystem->exists(self::$coreDevFolder)) {
-            $process = new ProcessExecutor();
-            $gitRemoteUrl = 'https://github.com/TYPO3/typo3.git';
-            $command = sprintf('git clone %s %s', ProcessExecutor::escape($gitRemoteUrl), ProcessExecutor::escape(self::$coreDevFolder));
-            self::message($event, '<info>Cloning TYPO3 repository. This may take a while depending on your internet connection!</info>');
-            $status = $process->executeTty($command);
+        $finder = Finder::create()
+            ->ignoreVCS(false)
+            ->ignoreDotFiles(false)
+            ->depth(0)
+            ->notName(['composer-repository.bak', '.DS_Store'])
+            ->in(self::$coreDevFolder);
 
-            if ($status) {
-                self::message($event, '<warning>Could not download git repository ' . $gitRemoteUrl . ' </warning>');
-                return false;
-            }
-        } else {
-            self::message($event, 'Repository exists! Therefore no download required.');
-            return true;
+        if (count($finder) > 0) {
+            self::message($event, sprintf("TDK: Directory <info>%s</info> is populated with unknown files. Please reset directory to vanilla state.", self::$coreDevFolder));
+            return false;
+        }
+
+        $filesystem = new Filesystem();
+        $filesystem->remove(self::$coreDevFolder);
+
+        $process = new ProcessExecutor();
+        $gitRemoteUrl = 'https://github.com/TYPO3/typo3.git';
+        $command = sprintf('git clone %s %s', ProcessExecutor::escape($gitRemoteUrl), ProcessExecutor::escape(self::$coreDevFolder));
+        self::message($event, '<info>Cloning TYPO3 repository. This may take a while depending on your internet connection!</info>');
+        $status = $process->executeTty($command);
+
+        if ($status) {
+            self::message($event, '<warning>Could not download git repository ' . $gitRemoteUrl . ' </warning>');
+            return false;
         }
 
         return true;
